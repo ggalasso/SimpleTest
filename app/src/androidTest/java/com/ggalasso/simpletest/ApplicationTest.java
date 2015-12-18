9cbe3a912f10f0f6a3b95bf0ef7f5aa0b6d49a35
ggalasso <ggalasso@gmail.com>
2015-12-17 21:41:58 -0600

    Finally added in some unit tests as a learning example for running unit tests in Android.

--- app/src/androidTest/java/com/ggalasso/simpletest/ApplicationTest.java
+++ app/src/androidTest/java/com/ggalasso/simpletest/ApplicationTest.java
@@ -3,11 +3,33 @@ package com.ggalasso.simpletest;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
+import com.ggalasso.simpletest.api.CollectionAPI;
+import com.ggalasso.simpletest.controller.GameIdManager;
+import com.ggalasso.simpletest.model.GameId;
+
+import java.util.ArrayList;
+
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
+    }
+
+    public void test() throws Exception {
+        final int expected = 5;
+        final int reality = 5;
+        assertEquals(expected, reality);
+
+    }
+
+    public void testRetrieveTwentyGames() throws Exception {
+        final int expected = 20;
+        CollectionAPI capi = new CollectionAPI();
+        GameIdManager gim = capi.getIDManager();
+        ArrayList<GameId> gameIds = gim.getGameIds();
+        final int reality = gameIds.size();
+        assertEquals(expected, reality);
     }
 }
