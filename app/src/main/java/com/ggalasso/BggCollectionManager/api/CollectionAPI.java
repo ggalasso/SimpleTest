855bd28cd233118e4ae19e0263e665d515058c29
Edward Castillo <e2thed@gmail.com>
2016-01-12 23:00:02 -0600

    Testing the committing with the application token

--- app/src/main/java/com/ggalasso/BggCollectionManager/api/CollectionAPI.java
+++ app/src/main/java/com/ggalasso/BggCollectionManager/api/CollectionAPI.java
@@ -14,7 +14,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.concurrent.ExecutionException;
 
-/**
+/***
  * Created by Edward on 9/8/2015.
  */
 public class CollectionAPI {
