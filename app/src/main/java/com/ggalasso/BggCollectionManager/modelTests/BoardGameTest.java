c6b283d7f96ec63416c4d1d9e133109a92744407
ggalasso <ggalasso@gmail.com>
2016-01-21 22:03:06 -0600

    Working on setting up JUnit tests for the BoardGame class. Just testing out running the tests for now will work on adding actual real tests soon.

---
+++ app/src/main/java/com/ggalasso/BggCollectionManager/modelTests/BoardGameTest.java
@@ -0,0 +1,55 @@
+package com.ggalasso.BggCollectionManager.modelTests;
+
+import com.ggalasso.BggCollectionManager.model.BoardGame;
+
+import junit.framework.TestCase;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.MockitoAnnotations.*;
+
+/**
+ * Created by truthd on 1/21/2016.
+ */
+public class BoardGameTest extends TestCase {
+
+    @Mock
+    private BoardGame bg;
+
+    @Before
+    public void setUp() throws Exception {
+        initMocks(this);
+        Mockito.when(bg.getId()).thenReturn("171");
+        Mockito.when(bg.getPrimaryName()).thenReturn("Chess");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPrimaryName() throws Exception {
+        assertEquals(bg.getPrimaryName(), "Chess");
+    }
+
+    @Test
+    public void testGetIdOption1() throws Exception {
+        BoardGame test = mock(BoardGame.class);
+        when(test.getId()).thenReturn("172");
+        assertEquals(test.getId(), "172");
+    }
+
+    @Test
+    public void testGetIdOption2() throws Exception {
+        assertEquals(bg.getId(), "171");
+    }
+
+
+}
