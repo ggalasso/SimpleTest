07727621f751be8e7370b89e0b822d208f156040
Edward Castillo <e2thed@gmail.com>
2016-02-08 22:25:22 -0600

    Tests - Category and Mechanic in game tests have moved to SQL Controller.

--- app/src/main/java/com/ggalasso/BggCollectionManagerUnitTests/dbTests/SQLControllerTest.java
+++ app/src/main/java/com/ggalasso/BggCollectionManagerUnitTests/dbTests/SQLControllerTest.java
@@ -1,6 +1,7 @@
 package com.ggalasso.BggCollectionManagerUnitTests.dbTests;
 
 import com.ggalasso.BggCollectionManager.db.CategoryInGameTable;
+import com.ggalasso.BggCollectionManager.db.SQLController;
 
 import junit.framework.Assert;
 
@@ -15,22 +16,28 @@ import static org.junit.Assert.*;
 /**
  * Created by Edward on 2/8/2016.
  */
-public class CategoryInGameTableTest extends CategoryInGameTable {
+public class SQLControllerTest extends SQLController {
 
     @Test
     public void testGetSQLInsertString() throws Exception {
         // Arrange
         Map<String,ArrayList<String>> bgCatMap = new HashMap<>();
+        ArrayList<String> columns = new ArrayList<>();
+        columns.add("cg_bg_Id");
+        columns.add("cg_ca_Id");
+        // Arrange Mock Values
         ArrayList<String> firstitemIds = new ArrayList<>();
         firstitemIds.add("9009");
         ArrayList<String> secondItemIds = new ArrayList<>();
         secondItemIds.add("9008");
         bgCatMap.put("1001", firstitemIds);
         bgCatMap.put("1002",secondItemIds);
+
+        // Arrange Expected String to compare
         String expected = "INSERT OR IGNORE INTO category_in_game (cg_bg_Id, cg_ca_Id) VALUES ('1002', '9008'),('1001', '9009');";
 
         // Act
-        String actual = super.getSQLInsertString(bgCatMap,"category_in_game","cg_bg_Id","cg_ca_Id");
+        String actual = super.getSQLInsertString(bgCatMap,"category_in_game",columns);
 
         // Assert
         Assert.assertEquals(expected, actual);
