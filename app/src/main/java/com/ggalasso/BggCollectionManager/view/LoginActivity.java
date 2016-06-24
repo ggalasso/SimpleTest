dbb3f5e8b4109df13ef2aa566c5bc92acdb63878
Edward Castillo <edwardcastillo@gmail.com>
2016-06-23 21:59:19 -0500

    Added code to display username text to log on button click

--- app/src/main/java/com/ggalasso/BggCollectionManager/view/LoginActivity.java
+++ app/src/main/java/com/ggalasso/BggCollectionManager/view/LoginActivity.java
@@ -1,10 +1,15 @@
 package com.ggalasso.BggCollectionManager.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.ggalasso.BggCollectionManager.R;
 
@@ -12,11 +17,21 @@ import com.ggalasso.BggCollectionManager.R;
  * Created by ggalasso on 6/16/2016.
  */
 public class LoginActivity extends AppCompatActivity{
+    Button loginButton;
+    EditText usernameTextBox;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        loginButton = (Button)findViewById(R.id.loginButton);
+        usernameTextBox = (EditText)findViewById(R.id.usernameTextBox);
+
+        loginButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Log.d("BGCM-LA","We Clickity Clicked the button: text = " + usernameTextBox.getText().toString());
+            }
+        });
     }
 
     @Override
